e6a9f0db7140f26381d43f529680284d0dfe58d6
Benjamin <benjamin.lewin@yahoo.com>
2023-04-03 14:32:42 -0400

    testing CRUD in front end

--- tickit/src/App.js
+++ tickit/src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, useParams} from 'react-router-dom'
+import Client from './services/api'
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [venueContent, setVenueContent] = useState([])
+
+  const { id } = useParams();
+
+  const getContent = () => {
+    Client.get(`/events`).then((getContent) => {
+      setVenueContent(getContent.data);
+    });
+  };
+
+  useEffect(() => {
+    getContent();
+  }, []);
+
+  const handleDelete = (id) => {
+    Client.delete(`/events/${id}`).then(() => {
+      getContent();
+    });
+    console.log(id)
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <div>
+        {venueContent.map((venue, id) => (
+          <div key={venue.id}>
+            <h2>{venue.artist}</h2>
+            <button onClick={() => handleDelete(venue.id)}>delete</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
