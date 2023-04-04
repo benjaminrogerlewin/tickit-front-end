50e1db34aaa81d6ed65fe76bdc36503dbcbe077f
Steph Perez <tephpez@gmail.com>
2023-04-04 13:40:11 -0400

    form initiated

--- tickit/src/components/Login.jsx
+++ tickit/src/components/Login.jsx
@@ -1,31 +1,67 @@
 import '../Modal.css';
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 import SignUp from './SignUp'
 
 
 function Login({setOpenLogin}) {
 
+    const userRef = useRef();
+
     const [registerOpen, setRegisterOpen] = useState(false);
     const [loginOpen, setLoginOpen] = useState(false);
+
+    const [user, setUser] = useState("");
+    const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [modalState, setModalState] = useState("block");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setUser("");
+        setPassword("");
+        setIsLoggedIn(true);
+      };
 
     return (
         <div className="modal-background">
             <section className="modal-container">
-                <button 
-                    className="close-modal"
-                    onClick={() => {setOpenLogin(false);}}
-                    >
-                    &times;
-                </button>
                 <div className="modal-header">
+                    <button 
+                        className="close-modal"
+                        onClick={() => {setOpenLogin(false);}}
+                        >
+                        &times;
+                    </button>
                     <h1 className="modal-title">LOGIN</h1>
                 </div>
                 <div className="modal-body">
-                    <p> FORM CONTENT GOES HERE </p>
+                    <form onSubmit={handleSubmit}>
+                    <input
+                        id="username"
+                        type="text"
+                        placeholder="username"
+                        ref={userRef}
+                        onChange={(e) => setUser(e.target.value)}
+                        required
+                    ></input>
+        
+                    <input
+                        type="password"
+                        placeholder="password"
+                        id="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                    </form>
                 </div>
                 <div className="modal-footer">
-                    <button>LOGIN</button>
+                    <button
+                        type="submit"
+                    >
+                        LOGIN
+                        </button>
                     <button 
                     className="primary-button" 
                     onClick={() => {
@@ -47,5 +83,4 @@ function Login({setOpenLogin}) {
 }
 
 export default Login;
-
 
