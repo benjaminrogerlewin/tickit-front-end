49b9d85fe626edc9646a7062cfa5bc377ec1e643
Fernando <me@fdorado.com>
2023-04-05 15:11:38 -0400

    Fixed CSS stuff.

--- tickit/src/components/AdminVenues.jsx
+++ tickit/src/components/AdminVenues.jsx
@@ -3,24 +3,33 @@ import Client from "../services/api";
 import { Link, useNavigate } from "react-router-dom";
 
 export default function AdminVenues(props) {
+  let navigate = useNavigate();
 
-    let navigate = useNavigate()
+  const showEvents = (ven) => {
+    console.log(ven.id);
+    navigate(`${ven.id}`);
+  };
 
-    const showEvents = (ven) => {
-        console.log(ven.id)
-        navigate(`${ven.id}`)
-    }
-
-    return(
-        <>
-            <h1>this is venues</h1>
-            <div>
-                {props.venue.map((ven) => (
-                    <div onClick={() => showEvents(ven)} key={ven.id}>
-                       <span>{ven.name}</span> 
-                    </div>
-                ))}
-            </div>
-        </>
-    )
+  return (
+    <div id="venues-container">
+      <h1>Venues</h1>
+      <div id="venue-cards-container">
+        {props.venue.map((ven) => (
+          <div
+            className="venue-card"
+            onClick={() => showEvents(ven)}
+            key={ven.id}
+          >
+            <h3>{ven.name}</h3>
+            <p>Address: {ven.address}</p>
+            <p>Phone: {ven.phone}</p>
+            <p>Capacity: {ven.capacity}</p>
+            {/* <p>Accessibility: {ven.accessible}</p> */}
+            <p>Parking: {ven.parking}</p>
+            <p>Hours: {ven.hours}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
